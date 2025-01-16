e2fd16de87ae1ebedec707cf045586c10ffda16d
nirjonroy <roynirjon18@gmail.com>
2025-01-16 13:46:20 +0600

    profile logout button issue solved

--- resources/views/frontend/cart/user-profile.blade.php
+++ resources/views/frontend/cart/user-profile.blade.php
@@ -66,7 +66,7 @@
   
       <!-- Logout Button -->
       <div class="text-right">
-        <button class="bg-red-600 text-white px-6 py-2 rounded-md hover:bg-red-500 transition duration-200">Logout</button>
+        <a href="{{url('logout')}}" class="bg-red-600 text-white px-6 py-2 rounded-md hover:bg-red-500 transition duration-200">Logout</a>
       </div>
       
     </div>
