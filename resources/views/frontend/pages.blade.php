d09e4597c7f2ff87a6c248a54c1047e009e046e2
nirjonroy <roynirjon18@gmail.com>
2025-01-16 22:11:41 +0600

    custom pages design change

--- resources/views/frontend/pages.blade.php
+++ resources/views/frontend/pages.blade.php
@@ -2,16 +2,34 @@
 @section('title')
 {{$customPage->page_name}}
 @endsection
-@push('css')
-<link rel="stylesheet" href="{{ asset('frontend/assets/css/cart.css') }}">
-@endpush
+
 
 @section('content')
-<div class="container">
-<h1>{{$customPage->page_name}}</h1>
-<p style="text-align:justify">{!!$customPage->description!!}</p>    
-</div>
 
+
+
+<main class="pt-32">
+
+    <div class="mb-4 md:mb-0 w-full max-w-screen-md mx-auto relative" style="height: 24em;">
+      <div class="absolute left-0 bottom-0 w-full h-full z-10"
+        ></div>
+      {{-- <img src="images/university/3.png" class="absolute left-0 top-0 w-full rounded-lg h-full z-0 object-cover" /> --}}
+      <div class="p-4 absolute bottom-0 left-0 z-20">
+       
+        <h2 class="text-4xl font-semibold text-black-800 leading-tight">
+            {{$customPage->page_name}}
+        </h2>
+        
+      </div>
+    </div>
+
+    <div class="px-4 lg:px-0 mt-12 text-gray-700 max-w-screen-md mx-auto text-lg leading-relaxed">
+      <p class="pb-6">{!!$customPage->description!!}</p>
+
+
+
+    </div>
+  </main>
 
 
 @endsection
